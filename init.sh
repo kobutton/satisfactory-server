e080453e41b0e72e0659c1b201677c8d0f12e2ca
Kyle Button <kyle@kobutton.com>
2023-02-28 10:31:56 -0500

    allow GID to be root

--- init.sh
+++ init.sh
@@ -47,21 +47,21 @@ mkdir -p \
     || exit 1
 
 # check if the user and group IDs have been set
-if ! [[ "$PGID" =~ $NUMCHECK ]] ; then
-    printf "Invalid group id given: %s\\n" "$PGID"
-    PGID="1000"
-elif [[ "$PGID" -eq 0 ]]; then
-    printf "PGID/group cannot be 0 (root)\\n"
-    exit 1
-fi
-
-if ! [[ "$PUID" =~ $NUMCHECK ]] ; then
-    printf "Invalid user id given: %s\\n" "$PUID"
-    PUID="1000"
-elif [[ "$PUID" -eq 0 ]]; then
-    printf "PUID/user cannot be 0 (root)\\n"
-    exit 1
-fi
+#if ! [[ "$PGID" =~ $NUMCHECK ]] ; then
+#    printf "Invalid group id given: %s\\n" "$PGID"
+#    PGID="1000"
+#elif [[ "$PGID" -eq 0 ]]; then
+#    printf "PGID/group cannot be 0 (root)\\n"
+#    #exit 1
+#fi
+#
+#if ! [[ "$PUID" =~ $NUMCHECK ]] ; then
+#    printf "Invalid user id given: %s\\n" "$PUID"
+#    PUID="1000"
+#elif [[ "$PUID" -eq 0 ]]; then
+#    printf "PUID/user cannot be 0 (root)\\n"
+#    exit 1
+#fi
 
 if [[ $(getent group $PGID | cut -d: -f1) ]]; then
     usermod -a -G "$PGID" steam
