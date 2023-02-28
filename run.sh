241166db5c1ad0a80e8792b635b1b8e60221f179
Kyle Button <kyle@kobutton.com>
2023-02-28 11:16:23 -0500

    using sed to replace ini values is super gross

--- run.sh
+++ run.sh
@@ -2,91 +2,91 @@
 
 set -e
 
-set_ini_prop() {
-    sed "/\[$2\]/,/^\[/ s/$3\=.*/$3=$4/" -i "/home/steam/$1"
-}
-
-set_ini_val() {
-    sed "/\[$2\]/,/^\[/ s/((\"$3\",.*))/((\"$3\", $4))/" -i "/home/steam/$1"
-}
-
-NUMCHECK='^[0-9]+$'
-
-## START Engine.ini
-if ! [[ "$AUTOSAVENUM" =~ $NUMCHECK ]] ; then
-    printf "Invalid autosave number given: %s\\n" "$AUTOSAVENUM"
-    AUTOSAVENUM="3"
-fi
-printf "Setting autosave number to %s\\n" "$AUTOSAVENUM"
-set_ini_prop "Engine.ini" "\/Script\/FactoryGame\.FGSaveSession" "mNumRotatingAutosaves" "$AUTOSAVENUM"
-
-[[ "${CRASHREPORT,,}" == "true" ]] && CRASHREPORT="true" || CRASHREPORT="false"
-printf "Setting crash reporting to %s\\n" "${CRASHREPORT^}"
-set_ini_prop "Engine.ini" "CrashReportClient" "bImplicitSend" "${CRASHREPORT^}"
-
-if ! [[ "$MAXOBJECTS" =~ $NUMCHECK ]] ; then
-    printf "Invalid max objects number given: %s\\n" "$MAXOBJECTS"
-    MAXOBJECTS="2162688"
-fi
-printf "Setting max objects number to %s\\n" "$MAXOBJECTS"
-set_ini_prop "Engine.ini" "\/Script\/Engine\.GarbageCollectionSettings" "gc.MaxObjectsInEditor" "$MAXOBJECTS"
-set_ini_prop "Engine.ini" "\/Script\/Engine\.GarbageCollectionSettings" "gc.MaxObjectsInGame" "$MAXOBJECTS"
-
-if ! [[ "$MAXTICKRATE" =~ $NUMCHECK ]] ; then
-    printf "Invalid max tick rate number given: %s\\n" "$MAXTICKRATE"
-    MAXTICKRATE="120"
-fi
-printf "Setting max tick rate to %s\\n" "$MAXTICKRATE"
-set_ini_prop "Engine.ini" "\/Script\/OnlineSubsystemUtils.IpNetDriver" "NetServerMaxTickRate" "$MAXTICKRATE"
-set_ini_prop "Engine.ini" "\/Script\/OnlineSubsystemUtils.IpNetDriver" "LanServerMaxTickRate" "$MAXTICKRATE"
-
-if ! [[ "$TIMEOUT" =~ $NUMCHECK ]] ; then
-    printf "Invalid timeout number given: %s\\n" "$TIMEOUT"
-    TIMEOUT="300"
-fi
-printf "Setting timeout number to %s\\n" "$TIMEOUT"
-set_ini_prop "Engine.ini" "\/Script\/OnlineSubsystemUtils\.IpNetDriver" "ConnectionTimeout" "$TIMEOUT"
-set_ini_prop "Engine.ini" "\/Script\/OnlineSubsystemUtils\.IpNetDriver" "InitialConnectTimeout" "$TIMEOUT"
-## END Engine.ini
-
-## START Game.ini
-if ! [[ "$MAXPLAYERS" =~ $NUMCHECK ]] ; then
-    printf "Invalid max players given: %s\\n" "$MAXPLAYERS"
-    MAXPLAYERS="4"
-fi
-printf "Setting max players to %s\\n" "$MAXPLAYERS"
-set_ini_prop "Game.ini" "\/Script\/Engine\.GameSession" "MaxPlayers" "$MAXPLAYERS"
-## END Game.ini
-
-## START GameUserSettings.ini
-if ! [[ "$AUTOSAVEINTERVAL" =~ $NUMCHECK ]] ; then
-    printf "Invalid autosave interval given: %s\\n" "$AUTOSAVEINTERVAL"
-    AUTOSAVEINTERVAL="300"
-fi
-printf "Setting autosave interval to %ss\\n" "$AUTOSAVEINTERVAL"
-set_ini_val "GameUserSettings.ini" "\/Script\/FactoryGame\.FGGameUserSettings" "FG.AutosaveInterval" "$AUTOSAVEINTERVAL"
-
-[[ "${DISABLESEASONALEVENTS,,}" == "true" ]] && DISABLESEASONALEVENTS="1" || DISABLESEASONALEVENTS="0"
-printf "Setting disable seasonal events to %s\\n" "$DISABLESEASONALEVENTS"
-set_ini_val "GameUserSettings.ini" "\/Script\/FactoryGame\.FGGameUserSettings" "FG.DisableSeasonalEvents" "$DISABLESEASONALEVENTS"
-
-if ! [[ "$NETWORKQUALITY" =~ $NUMCHECK ]] ; then
-    printf "Invalid network quality number given: %s\\n" "$NETWORKQUALITY"
-    NETWORKQUALITY="3"
-fi
-printf "Setting network quality number to %s\\n" "$NETWORKQUALITY"
-set_ini_prop "GameUserSettings.ini" "\/Script\/FactoryGame\.FGGameUserSettings" "mNetworkQuality" "$NETWORKQUALITY"
-## END GameUserSettings.ini
-
-## START ServerSettings.ini
-[[ "${AUTOPAUSE,,}" == "true" ]] && AUTOPAUSE="true" || AUTOPAUSE="false"
-printf "Setting auto pause to %s\\n" "${AUTOPAUSE^}"
-set_ini_prop "ServerSettings.ini" "\/Script\/FactoryGame\.FGServerSubsystem" "mAutoPause" "${AUTOPAUSE^}"
-
-[[ "${AUTOSAVEONDISCONNECT,,}" == "true" ]] && AUTOSAVEONDISCONNECT="true" || AUTOSAVEONDISCONNECT="false"
-printf "Setting autosave on disconnect to %s\\n" "${AUTOSAVEONDISCONNECT^}"
-set_ini_prop "ServerSettings.ini" "\/Script\/FactoryGame\.FGServerSubsystem" "mAutoSaveOnDisconnect" "${AUTOSAVEONDISCONNECT^}"
-## END ServerSettings.ini
+#set_ini_prop() {
+#    sed "/\[$2\]/,/^\[/ s/$3\=.*/$3=$4/" -i "/home/steam/$1"
+#}
+#
+#set_ini_val() {
+#    sed "/\[$2\]/,/^\[/ s/((\"$3\",.*))/((\"$3\", $4))/" -i "/home/steam/$1"
+#}
+#
+#NUMCHECK='^[0-9]+$'
+#
+### START Engine.ini
+#if ! [[ "$AUTOSAVENUM" =~ $NUMCHECK ]] ; then
+#    printf "Invalid autosave number given: %s\\n" "$AUTOSAVENUM"
+#    AUTOSAVENUM="3"
+#fi
+#printf "Setting autosave number to %s\\n" "$AUTOSAVENUM"
+#set_ini_prop "Engine.ini" "\/Script\/FactoryGame\.FGSaveSession" "mNumRotatingAutosaves" "$AUTOSAVENUM"
+#
+#[[ "${CRASHREPORT,,}" == "true" ]] && CRASHREPORT="true" || CRASHREPORT="false"
+#printf "Setting crash reporting to %s\\n" "${CRASHREPORT^}"
+#set_ini_prop "Engine.ini" "CrashReportClient" "bImplicitSend" "${CRASHREPORT^}"
+#
+#if ! [[ "$MAXOBJECTS" =~ $NUMCHECK ]] ; then
+#    printf "Invalid max objects number given: %s\\n" "$MAXOBJECTS"
+#    MAXOBJECTS="2162688"
+#fi
+#printf "Setting max objects number to %s\\n" "$MAXOBJECTS"
+#set_ini_prop "Engine.ini" "\/Script\/Engine\.GarbageCollectionSettings" "gc.MaxObjectsInEditor" "$MAXOBJECTS"
+#set_ini_prop "Engine.ini" "\/Script\/Engine\.GarbageCollectionSettings" "gc.MaxObjectsInGame" "$MAXOBJECTS"
+#
+#if ! [[ "$MAXTICKRATE" =~ $NUMCHECK ]] ; then
+#    printf "Invalid max tick rate number given: %s\\n" "$MAXTICKRATE"
+#    MAXTICKRATE="120"
+#fi
+#printf "Setting max tick rate to %s\\n" "$MAXTICKRATE"
+#set_ini_prop "Engine.ini" "\/Script\/OnlineSubsystemUtils.IpNetDriver" "NetServerMaxTickRate" "$MAXTICKRATE"
+#set_ini_prop "Engine.ini" "\/Script\/OnlineSubsystemUtils.IpNetDriver" "LanServerMaxTickRate" "$MAXTICKRATE"
+#
+#if ! [[ "$TIMEOUT" =~ $NUMCHECK ]] ; then
+#    printf "Invalid timeout number given: %s\\n" "$TIMEOUT"
+#    TIMEOUT="300"
+#fi
+#printf "Setting timeout number to %s\\n" "$TIMEOUT"
+#set_ini_prop "Engine.ini" "\/Script\/OnlineSubsystemUtils\.IpNetDriver" "ConnectionTimeout" "$TIMEOUT"
+#set_ini_prop "Engine.ini" "\/Script\/OnlineSubsystemUtils\.IpNetDriver" "InitialConnectTimeout" "$TIMEOUT"
+### END Engine.ini
+#
+### START Game.ini
+#if ! [[ "$MAXPLAYERS" =~ $NUMCHECK ]] ; then
+#    printf "Invalid max players given: %s\\n" "$MAXPLAYERS"
+#    MAXPLAYERS="4"
+#fi
+#printf "Setting max players to %s\\n" "$MAXPLAYERS"
+#set_ini_prop "Game.ini" "\/Script\/Engine\.GameSession" "MaxPlayers" "$MAXPLAYERS"
+### END Game.ini
+#
+### START GameUserSettings.ini
+#if ! [[ "$AUTOSAVEINTERVAL" =~ $NUMCHECK ]] ; then
+#    printf "Invalid autosave interval given: %s\\n" "$AUTOSAVEINTERVAL"
+#    AUTOSAVEINTERVAL="300"
+#fi
+#printf "Setting autosave interval to %ss\\n" "$AUTOSAVEINTERVAL"
+#set_ini_val "GameUserSettings.ini" "\/Script\/FactoryGame\.FGGameUserSettings" "FG.AutosaveInterval" "$AUTOSAVEINTERVAL"
+#
+#[[ "${DISABLESEASONALEVENTS,,}" == "true" ]] && DISABLESEASONALEVENTS="1" || DISABLESEASONALEVENTS="0"
+#printf "Setting disable seasonal events to %s\\n" "$DISABLESEASONALEVENTS"
+#set_ini_val "GameUserSettings.ini" "\/Script\/FactoryGame\.FGGameUserSettings" "FG.DisableSeasonalEvents" "$DISABLESEASONALEVENTS"
+#
+#if ! [[ "$NETWORKQUALITY" =~ $NUMCHECK ]] ; then
+#    printf "Invalid network quality number given: %s\\n" "$NETWORKQUALITY"
+#    NETWORKQUALITY="3"
+#fi
+#printf "Setting network quality number to %s\\n" "$NETWORKQUALITY"
+#set_ini_prop "GameUserSettings.ini" "\/Script\/FactoryGame\.FGGameUserSettings" "mNetworkQuality" "$NETWORKQUALITY"
+### END GameUserSettings.ini
+#
+### START ServerSettings.ini
+#[[ "${AUTOPAUSE,,}" == "true" ]] && AUTOPAUSE="true" || AUTOPAUSE="false"
+#printf "Setting auto pause to %s\\n" "${AUTOPAUSE^}"
+#set_ini_prop "ServerSettings.ini" "\/Script\/FactoryGame\.FGServerSubsystem" "mAutoPause" "${AUTOPAUSE^}"
+#
+#[[ "${AUTOSAVEONDISCONNECT,,}" == "true" ]] && AUTOSAVEONDISCONNECT="true" || AUTOSAVEONDISCONNECT="false"
+#printf "Setting autosave on disconnect to %s\\n" "${AUTOSAVEONDISCONNECT^}"
+#set_ini_prop "ServerSettings.ini" "\/Script\/FactoryGame\.FGServerSubsystem" "mAutoSaveOnDisconnect" "${AUTOSAVEONDISCONNECT^}"
+### END ServerSettings.ini
 
 if ! [[ "${SKIPUPDATE,,}" == "true" ]]; then
     if [[ "${STEAMBETA,,}" == "true" ]]; then
